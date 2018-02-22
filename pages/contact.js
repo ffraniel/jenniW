ebb626a2ae73597ee58030599e2dad89cbfc7814
ffraniel <francis.whitehead@googlemail.com>
2018-02-22 14:23:14 +0000

    added contact page styling

--- pages/contact.js
+++ pages/contact.js
@@ -12,6 +12,17 @@ const Contact = (props) => (
                         <main className="content" >
                             <h1>Contact page</h1>
                             <p>You can contact Jenni at....</p>
+                            <a href="genni@postmaster.co.uk">genni@postmaster.co.uk</a>
+                            <form id="contactForm">
+                                <p>Name: </p><input type="text" placeholder="name" />
+                                <br />
+                                <p>Email: </p><input type="text" placeholder="email" />
+                                <br />
+                                <p>Organisation: </p><input type="text" placeholder="organisation" />
+                                <br />
+                                <p>Message: </p><textarea placeholder="message" id="messageInput" maxLength="500" />
+                                {/* // limit at 500 words */}
+                            </form>
                         </main>
                     </div>
                 </Layout>
@@ -35,6 +46,33 @@ const Contact = (props) => (
                     .contact .content p {
                         font-family:var(--thickFont);
                         font-size:var(--smallFont);
+                    }
+                    #contactForm {
+                        padding:4px;
+
+                    }
+                    #contactForm input {
+                        border: 3px solid #cccccc;
+                        border-radius:5px;
+                        padding: 5px;
+                        font-family: var(--thickFont);
+                        font-color:black; 
+                        font-size:var(--referenceSize);
+                        width:80%;
+	                    min-height: 20px; 
+                    }
+                    #messageInput {
+                        resize: none;
+                        overflow: auto;
+                        height: auto; 
+                        width:80%;
+	                    min-height: 80px;
+                        border: 3px solid #cccccc;
+                        border-radius:5px;
+                        padding: 5px;
+                        font-size:var(--referenceSize);
+                        font-family: var(--thickFont);
+                        font-color:black;  
                     }
                     `}
 
