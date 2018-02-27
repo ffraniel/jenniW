e7b39c0fb8d7af8df03955be9e7c6b0363ee3cb6
ffraniel <francis.whitehead@googlemail.com>
2018-02-27 21:27:37 +0000

    updated api access point at Prismic.io

--- .next/dist/config.js
+++ .next/dist/config.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var config = {
-    apiEndpoint: "https://JenniW.prismic.io/api/v2",
+    apiEndpoint: "https://jenniw.cdn.prismic.io/api/v2",
     accessToken: "MC5Xb0Y0QVNnQUFEYWtveTRM.77-977-9ZR7vv73vv73vv73vv70KNg3vv73vv73vv71NIu-_vTZEN--_ve-_vUHvv70tNUTvv73vv73vv73vv73vv70"
 };
 
